3569ee6c4c3d28c70b691c260d251f2ecd614474
ShailiManamperi <shailimanamperi2002@gmail.com>
2023-11-22 17:05:55 +0530

    add arraylist demo

---
+++ src/lk/ijse/gdse/ds/linkedlist/ArrayListDemo.java
@@ -0,0 +1,18 @@
+package lk.ijse.gdse.ds.linkedlist;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+public class ArrayListDemo {
+    public static void main(String[] args) {
+        List<Integer> list = new ArrayList<>();
+
+        list.add(10);
+        list.add(20);
+        list.add(30);
+
+        System.out.println(list);
+
+    }
+}
