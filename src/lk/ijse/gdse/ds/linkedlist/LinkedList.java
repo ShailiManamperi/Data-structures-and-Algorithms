1fdfcb78e84752228cf45f0e9cf698fc65e1743e
ShailiManamperi <shailimanamperi2002@gmail.com>
2023-11-22 14:09:29 +0530

    add LinkedList structure

---
+++ src/lk/ijse/gdse/ds/linkedlist/LinkedList.java
@@ -0,0 +1,13 @@
+package lk.ijse.gdse.ds.linkedlist;
+
+public class LinkedList {
+    private static class Node{
+        private int data;
+        private Node next;
+
+        public Node(int data){
+            this.data=data;
+        }
+    }
+    private Node head;
+}
---
+++ src/lk/ijse/gdse/ds/linkedlist/Main.java
@@ -0,0 +1,7 @@
+package lk.ijse.gdse.ds.linkedlist;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
