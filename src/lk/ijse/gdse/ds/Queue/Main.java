659cebf85b954217d5d51393c6b0037ce2ccbd97
ShailiManamperi <shailimanamperi2002@gmail.com>
2023-11-22 12:57:30 +0530

    add queue strucure

---
+++ src/lk/ijse/gdse/ds/Queue/Main.java
@@ -0,0 +1,10 @@
+package lk.ijse.gdse.ds.Queue;
+
+public class Main {
+    public static void main(String[] args) {
+        queue q1 = new queue(5);
+
+//        q1.enQueue(10);
+//        q1.enQueue(20);
+    }
+}
---
+++ src/lk/ijse/gdse/ds/Queue/queue.java
@@ -0,0 +1,16 @@
+package lk.ijse.gdse.ds.Queue;
+
+public class queue {
+    int[] elemenetdata;
+    int froent = -1;
+    int rear = -1;
+
+    public queue(int capacity){
+        elemenetdata = new int[capacity];
+    }
+
+
+
+
+
+}
