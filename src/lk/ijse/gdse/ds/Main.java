d1e455559647690c6b2d28a9f18218323a99fd0b
ShailiManamperi <shailimanamperi2002@gmail.com>
2023-11-22 10:31:12 +0530

    add peek method

--- src/lk/ijse/gdse/ds/Main.java
+++ src/lk/ijse/gdse/ds/Main.java
@@ -2,19 +2,19 @@ package lk.ijse.gdse.ds;
 
 public class Main {
     public static void main(String[] args) {
-        stack s1 = new stack(10);
+        stack stack = new stack(10);
 
         stack.push(10);
         stack.push(20);
         stack.push(30);
 
-//        int peek = stack.peek();
-//        System.out.println(peek);
-//
-//        stack.push(40);
-//        System.out.println(stack.peek());
-//
-//        stack.push(50);
-//        System.out.println(stack.peek());
+        int peek = stack.peek();
+        System.out.println(peek);
+
+        stack.push(40);
+        System.out.println(stack.peek());
+
+        stack.push(50);
+        System.out.println(stack.peek());
     }
 }
--- src/lk/ijse/gdse/ds/stack.java
+++ src/lk/ijse/gdse/ds/stack.java
@@ -1,16 +1,20 @@
 package lk.ijse.gdse.ds;
 
 public class stack {
-    private static int[] elementData;
-    private static int top;  //pointer to the top of the stack
+    private int[] elementData;
+    private int top;  //pointer to the top of the stack
 
     public stack(int initicalCapacity) {
         elementData = new int[initicalCapacity];
         top =-1;
     }
 
-    public static void push(int data){
+    public  void push(int data){
         elementData[++top] =data;
+    }
+
+    public int peek(){
+        return elementData[top];
     }
 
 
