16fad084ec0d259c4c8f5dd0cf9fddd247829102
ShailiManamperi <shailimanamperi2002@gmail.com>
2023-11-22 10:26:50 +0530

    Adding project

---
+++ src/lk/ijse/gdse/ds/stack.java
@@ -0,0 +1,15 @@
+package lk.ijse.gdse.ds;
+
+public class stack {
+    private static int[] elementData;
+    private static int top;  //pointer to the top of the stack
+
+    public stack(int initicalCapacity) {
+        elementData = new int[initicalCapacity];
+        top =-1;
+    }
+
+
+
+
+}
