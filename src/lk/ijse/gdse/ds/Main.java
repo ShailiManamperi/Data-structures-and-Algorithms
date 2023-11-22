0a04b5f5188f21bcd82680c3176e87d80105bc86
ShailiManamperi <shailimanamperi2002@gmail.com>
2023-11-22 10:55:41 +0530

    update with output in comment

--- src/lk/ijse/gdse/ds/Main.java
+++ src/lk/ijse/gdse/ds/Main.java
@@ -9,23 +9,28 @@ public class Main {
         stack.push(30);
 
         int peek = stack.peek();
-        System.out.println(peek);
+        System.out.println(peek);  //30
 
         stack.push(40);
-        System.out.println(stack.peek());
+        System.out.println(stack.peek()); //40
 
         stack.push(50);
-        System.out.println(stack.peek());
+        System.out.println(stack.peek());  // 50
 
         int pop = stack.pop();
-        System.out.println("return value "+pop);
+        System.out.println("return value "+pop); //return value 50
+        System.out.println("top value "+stack.peek()); //top value 40
 
-        System.out.println("return value "+stack.pop());
+        System.out.println("return value "+stack.pop()); //return value 40
+        System.out.println("top value "+stack.peek());   //top value 30
 
-        stack.printstack();
+        stack.printstack(); //[ 10 , 20 , 30 ]
 
         stack.push(400);// replace the 400 for the 40 . because still that 3 elememt has 40 value it get replace by 400.
-        stack.printstack();
+
+        System.out.println(stack.peek()); //400
+
+        stack.printstack(); // [ 10 , 20 , 30 , 400 ]
 
     }
 }
